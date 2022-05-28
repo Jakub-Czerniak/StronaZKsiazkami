969ca8c175e9c1ff651d10de08b5d1f0114bbf10
robotanical <94900199+robotanical@users.noreply.github.com>
2022-05-28 10:49:36 +0200

    ad dl OrderModel

--- DataLibrary/Models/OrderModel.cs
+++ DataLibrary/Models/OrderModel.cs
@@ -8,5 +8,15 @@ namespace DataLibrary.Models
 {
     internal class OrderModel
     {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }
+        public string Adress { get; set; }
+        public string Apartment { get; set; }
+        public string Postcode { get; set; }
+        public System.DateTime OrderDate { get; set; }
+        public decimal TotalCost { get; set; }
+        public string Status { get; set; }
     }
 }
