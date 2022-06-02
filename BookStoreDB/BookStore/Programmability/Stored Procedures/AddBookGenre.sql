539d6428760d0ca4225146f765c6ca6dfec105cc
robotanical <94900199+robotanical@users.noreply.github.com>
2022-06-02 09:54:00 +0200

    insert bookgenre only if it doesnt exist

--- BookStoreDB/BookStore/Programmability/Stored Procedures/AddBookGenre.sql
+++ BookStoreDB/BookStore/Programmability/Stored Procedures/AddBookGenre.sql
@@ -2,5 +2,11 @@
 	@book_id int = 0,
 	@genre_id int =0
 AS
+IF NOT EXISTS
+(
+SELECT * WHERE books_genres.book_id = @book_id AND books_genres.genre_id = @genre_id
+)
+BEGIN
 	INSERT INTO books_genres(genre_id, book_id) VALUES( @genre_id, @book_id)
+END
 RETURN 0
